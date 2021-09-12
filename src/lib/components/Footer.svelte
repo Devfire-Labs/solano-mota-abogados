8a93ad9e9a370d7e894f4ca69ce8e65ed697aa3e
Luis Bonilla <blessedluismi@gmail.com>
2021-09-11 20:53:42 -0400

    minor updates, social logo changes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -14,9 +14,9 @@
 </script>
 
 <footer class="px-8 pt-10 bg-primary lg:grid-rows-2">
-  <div class="lg:flex lg:place-self-start">
-    <div class="lg:order-4">
-      <h3 class="text-white font-galey">
+  <div class="lg:flex lg:place-self-start lg:justify-between">
+    <div class="lg:ml-32 lg:order-4">
+      <h3 class="w-3/4 text-white font-galey">
         Subscríbete para recibir nuestras promociones y ofertas por correo
         electrónico:
       </h3>
@@ -34,31 +34,36 @@
       </form>
     </div>
 
-    <div class="lg:order-1">
+    <div class="lg:order-1 lg:px-10 lg:w-1/3">
       <a href="/" class="block mx-auto">
         <img src="/assets/logo.svg" alt="" class="mx-auto mt-10" />
       </a>
-      <div class="flex justify-between w-3/5 mx-auto mt-5 lg:w-1/3">
-        <a href=""
-          ><img src="/assets/twitter.svg" alt="Twitter icon" class="" /></a
+
+      <div class="flex justify-between w-3/5 mx-auto mt-5 lg:w-1/2">
+        <a href="/" class=""
+          ><img src="/assets/twitter.svg" alt="Twitter icon" class="w-10" /></a
         >
-        <as class=""
-          ><img src="/assets/instagram.svg" alt="Instagram icon" class="" /></as
+        <as class="ml-2"
+          ><img
+            src="/assets/instagram.svg"
+            alt="Instagram icon"
+            class="w-10"
+          /></as
         >
-        <a href=""
+        <a href="/" class="ml-2"
           ><img src="/assets/linkedin.svg" alt="LinkedIn icon" class="" /></a
         >
       </div>
     </div>
     <div
-      class="flex flex-wrap text-white mt-7 lg:order-2 lg:flex-col lg:pl-4 lg:pr-8"
+      class="flex flex-wrap text-white mt-7 lg:order-2 lg:flex-col lg:w-1/3 lg:ml-16 lg:text-left"
     >
       <a href="/" class="footer-link">Inicio</a>
       <a href="/nosotros" class="footer-link">Sobre Nosotros</a>
       <a href="/servicios" class="footer-link">Servicios</a>
       <a href="/contacto" class="footer-link">Contacto</a>
     </div>
-    <div class="mt-5 text-left text-white font-galey lg:order-3">
+    <div class="mt-5 text-left text-white lg:ml-16 font-galey lg:order-3">
       {#each info as { img: { src, alt }, text, href }, i (i)}
         {#if href}
           <div class="flex mb-3">
@@ -88,7 +93,7 @@
 
 <style>
   .footer-link {
-    @apply w-1/2;
+    @apply w-1/2 mt-2;
   }
 
   h3 {
