d4c53b0111b4395e9b4e41b435fc3ca01599364f
Luis Bonilla <blessedluismi@gmail.com>
2021-09-14 09:39:38 -0400

    Fixed servicios section

--- src/lib/components/ServiceCard.svelte
+++ src/lib/components/ServiceCard.svelte
@@ -1,19 +1,19 @@
 <script>
-	export let title, body, img, list;
-	let { src, alt } = img;
+  export let title, body, img, list;
+  let { src, alt } = img;
 </script>
 
-<div class="bg-secondary text-white py-6 px-5 mt-6 text-left">
-	<img {src} {alt} class="" />
-	<h3 class="text-xl mt-4">{title}</h3>
-	<p class="mt-6 leading-6">
-		{body}
-		{#if list}
-			<ul class="list-disc list-inside">
-				{#each list as item, i (i)}
-					<li class="mt-4" class:mt-2={i > 0}>{item}</li>
-				{/each}
-			</ul>
-		{/if}
-	</p>
+<div class="px-5 py-6 mt-6 text-left text-white bg-secondary">
+  <img {src} {alt} class="w-6 lg:w-12" />
+  <h3 class="mt-4 text-xl">{title}</h3>
+  <p class="mt-6 leading-6">
+    {body}
+    {#if list}
+      <ul class="list-disc list-inside">
+        {#each list as item, i (i)}
+          <li class="mt-4" class:mt-2={i > 0}>{item}</li>
+        {/each}
+      </ul>
+    {/if}
+  </p>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,17 @@
 <script context="module">
-	import ContactUs from '$lib/components/ContactUs.svelte';
-	import Hero from '$lib/components/Hero.svelte';
-	import Services from '$lib/components/Services.svelte';
-	import WhyWorkWithUs from '$lib/components/WhyWorkWithUs.svelte';
+  import ContactUs from "$lib/components/ContactUs.svelte";
+  import Hero from "$lib/components/Hero.svelte";
+  import Services from "$lib/components/Services.svelte";
+  import WhyWorkWithUs from "$lib/components/WhyWorkWithUs.svelte";
 
-	export const prerender = true;
-
+  export const prerender = true;
 </script>
 
 <svelte:head><title>Home</title></svelte:head>
 
 <main>
-	<Hero />
-	<Services />
-	<WhyWorkWithUs />
-	<ContactUs />
+  <Hero />
+  <Services />
+  <WhyWorkWithUs />
+  <ContactUs />
 </main>
-
-<!-- <style>
-	h2 {
-		@apply lg:text-2xl;
-	}
-
-</style> -->
